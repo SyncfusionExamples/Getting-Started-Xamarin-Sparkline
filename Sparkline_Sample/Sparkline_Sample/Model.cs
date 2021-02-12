3e0e39784d32e2e4aca72291c90b78c09f435aa7
AzureAD\SrideviSivakumar <AzureAD\sridevi.sivakumar@syncfusion.com>
2021-02-12 13:54:12 +0530

    Added sample

---
+++ Sparkline_Sample/Sparkline_Sample/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Sparkline_Sample
+{
+    public class Model
+    {
+        public double Performance { get; set; }
+    }
+}
---
+++ Sparkline_Sample/Sparkline_Sample/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Sparkline_Sample
+{
+    public class ViewModel
+    {
+        public List<Model> Data { get; set; }
+        public ViewModel()
+        {
+            Data = new List<Model>();
+            Data.Add(new Model() { Performance = 3000 });
+            Data.Add(new Model() { Performance = 5000 });
+            Data.Add(new Model() { Performance = -3000 });
+            Data.Add(new Model() { Performance = -4000 });
+            Data.Add(new Model() { Performance = 2000 });
+            Data.Add(new Model() { Performance = 3000 });
+        }
+    }
+}
